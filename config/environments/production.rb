eb14a44f0f523a4c0e2c2c323792805742588b8a
Md Ohidurzaman <zamanohidur@yahoo.com>
2018-08-18 03:33:25 +0000

    Email set up with SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'management-app-zaman.herokuapp.com/', :protocol => 'https'}
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
